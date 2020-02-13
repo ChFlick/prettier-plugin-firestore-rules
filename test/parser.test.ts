5b6c8939e4927b07ae23b75a522fdaa77566fbd3
Christoph Flick <christophflick@gmx.de>
2020-02-13 08:57:51 +0100

    Add a more complex parser test

--- test/parser.test.ts
+++ test/parser.test.ts
@@ -1,5 +1,7 @@
-import { parse } from '../src/parser/parser';
 import { expect } from 'chai';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+import { parse } from '../src/parser/parser';
 
 describe('the parser', () => {
     it('generates a valid result', () => {
@@ -13,6 +15,15 @@ describe('the parser', () => {
         }`;
 
         const result = parse(basicRule);
+
+        expect(result).to.be.an('array').with.length.gt(0);
+    });
+
+    test('can parse the example rules', () => {
+        const exampleRulesPath = resolve(__dirname + '/../example.rules');
+        const exampleRules = readFileSync(exampleRulesPath).toString();
+
+        const result = parse(exampleRules);
 
         expect(result).to.be.an('array').with.length.gt(0);
     });
